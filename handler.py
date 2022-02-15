24c285f142bf850e1da4fa9095fba528ee2f5b07
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-02-15 12:30:50 -1000

    * Add back requirements files.

--- handler.py
+++ handler.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import typer
 from panoptes.utils.images import make_pretty_image, cr2, fits as fits_utils
 from watchdog.events import FileSystemEventHandler
 
@@ -11,30 +10,30 @@ class Handler(FileSystemEventHandler):
     def on_any_event(event, **kwargs):
         if event.event_type == 'created':
             filename = Path(event.src_path)
-            typer.echo(f"Received created event for {filename}.")
+            print(f"Received created event for {filename}.")
 
             file_type = filename.suffix.lower()
 
             # Convert CR2
             if file_type == '.cr2':
-                typer.echo('Getting thumbnail from CR2')
+                print('Getting thumbnail from CR2')
                 thumb_path = make_pretty_image(str(filename))
-                typer.echo(f'Thumbnail saved to {thumb_path=}')
+                print(f'Thumbnail saved to {thumb_path=}')
 
-                typer.echo(f'Converting CR2 to FITS for {filename}')
+                print(f'Converting CR2 to FITS for {filename}')
                 cr2.cr2_to_fits(str(filename), remove_cr2=True)
 
             if file_type == '.fits':
-                typer.echo(f'Plate solving {filename}')
+                print(f'Plate solving {filename}')
                 try:
                     wcs0 = fits_utils.getwcs(str(filename))
                     if wcs0.is_celestial is False:
                         metadata = fits_utils.get_solve_field(filename)
                         filename = metadata['solved_fits_file']
-                        typer.echo(f'Solved {filename}')
+                        print(f'Solved {filename}')
                 except Exception as e:
-                    typer.echo(f'Problem solving {filename=}: {e!r}')
+                    print(f'Problem solving {filename=}: {e!r}')
 
-                typer.echo(f'Compressing FITS file')
+                print(f'Compressing FITS file')
                 filename = fits_utils.fpack(filename)
-                typer.echo(f'Solved and compressed file created: {filename=}')
+                print(f'Solved and compressed file created: {filename=}')
