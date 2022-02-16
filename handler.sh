81df756bf5dba90806dc1aacabfbf6220401d701
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-02-16 11:17:49 -1000

    Simplify some of the solve options.

--- handler.sh
+++ handler.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 OUTGOING_DIR="${OUTGOING_DIR:-/outgoing}"
-SOLVE_OPTS="${SOLVE_OPTS:---guess-scale --no-verify --crpix-center --temp-axy --downsample 4 --no-plots --overwrite}"
+SOLVE_OPTS="${SOLVE_OPTS:-}"
 
 # Comes from inotifywait. See watcher.sh
 datetime=$1
@@ -10,5 +10,5 @@ filename=$3
 event=$4
 
 if [ ${filename##*.} == 'fits' ]; then
-  /usr/bin/solve-field "${SOLVE_OPTS}" --dir "${OUTGOING_DIR}" "${filename}"
+  /usr/bin/solve-field "${SOLVE_OPTS} --dir ${OUTGOING_DIR} ${filename}"
 fi
